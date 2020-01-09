74d41cb1937e924ba74486e2a206d2452645f184
ebrahim-elias <ebrahim.elias@outlook.com>
2020-01-09 20:46:38 +0100

    chenge from array to arraylist for deck

--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace CardGame
 {
     class Deck
     {
-        private Card[] deck;
+        private ArrayList deck;
         private const int NUMBER_OF_CARDS = 52;
 
         public Deck()
         {
             string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             string[] suits = { "Hearts", "Clubs", "Diamonds", "Spades" };
-            deck = new Card[NUMBER_OF_CARDS];
+            deck = new ArrayList(NUMBER_OF_CARDS);
             int count = 0;
             while (count < NUMBER_OF_CARDS)
             {
@@ -23,8 +24,12 @@ namespace CardGame
                 {
                     foreach (var face in faces)
                     {
-                        deck[count] = new Card(face, suit);
-                        Console.WriteLine(deck[count]);
+                        deck.Add(new Card(face, suit));
+                        Console.Write(deck[count]+", ");
+                        if ((count+1) % 4 == 0)
+                        {
+                            Console.WriteLine();
+                        }
                         count++;
                     }
                 }
