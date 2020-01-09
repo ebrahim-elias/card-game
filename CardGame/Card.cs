3ea671e349d75723d64736131667b8f45563e04c
ebrahim-elias <ebrahim.elias@outlook.com>
2020-01-09 19:32:53 +0100

    adding card class and deck class with filling the deck  cards (52)

---
+++ CardGame/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Card
+    {
+        private string face;
+        private string suit;
+
+        public Card(string face, string suit)
+        {
+            this.face = face;
+            this.suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return face + " of " + suit;
+        }
+    }
+}
