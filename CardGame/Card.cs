fbde7dbd5516cb4e30977e6f21fbc2c22d5cb334
ebrahim-elias <ebrahim.elias@outlook.com>
2020-01-09 22:54:01 +0100

    adding documentation

--- CardGame/Card.cs
+++ CardGame/Card.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/**
+* Card class represents a playing card
+*/
 
 namespace CardGame
 {
     class Card
     {
-        private string face;
-        private string suit;
-
+        private string face; // like Ace, king ..
+        private string suit; // like hearts, clubs ..
+        
+        //constructor takes two parameters to initializes a card
         public Card(string face, string suit)
         {
             this.face = face;
             this.suit = suit;
         }
 
+        // override tostring method for custom showing
         public override string ToString()
         {
             return face + " of " + suit;
--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -4,38 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
-
+/**
+ * Deck class represents a deck of the playing cards 
+ */
 namespace CardGame
 {
     class Deck
     {
-        private ArrayList deck;
-        private const int NUMBER_OF_CARDS = 52;
+        private ArrayList deck; // Array list of the card objects
+        private const int NUMBER_OF_CARDS = 52; 
 
+        // constructor fills the deck
         public Deck()
         {
             string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             string[] suits = { "Hearts", "Clubs", "Diamonds", "Spades" };
-            deck = new ArrayList(NUMBER_OF_CARDS);
-            int count = 0;
+            deck = new ArrayList(NUMBER_OF_CARDS); // create the list based on the cards number
+            int count = 0;    // init counter to iterate the deck 
             while (count < NUMBER_OF_CARDS)
             {
-                foreach (var suit in suits)
+                foreach (var suit in suits) // first outerloop that iterate the suits  
                 {
-                    foreach (var face in faces)
+                    foreach (var face in faces) // innerloop for iterating the faces for each suit
                     {
-                        deck.Add(new Card(face, suit));
+                        deck.Add(new Card(face, suit)); // create a new card and add it to the deck list
+                        // test the output 
                         Console.Write(deck[count]+", ");
                         if ((count+1) % 4 == 0)
                         {
                             Console.WriteLine();
                         }
-                        count++;
+                        count++; 
                     }
                 }
                 
             }
             
+        }
+        // Mix method to shuffle the cards
+        public void mix()
+        {
+
         }
     }
 }
